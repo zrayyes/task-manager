d81f967fcc4b1d54ae16dfc198343a0f8af6a02a
Zuhair Rayyes <zuhair@rocketloop.de>
2025-03-25 22:45:01 +0200

    setup database connection and add middleware for logging and recovery

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
-	"net/http"
 
 	"github.com/zrayyes/task-manager/internal/repositories"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -27,21 +28,40 @@ func NewService() *Service {
 	}
 }
 
-// HelloWorld is a basic handler function
-func (s *Service) HelloWorld(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
-}
-
 func main() {
-	e := echo.New()
+	// Setup database connection
+	connStr := "postgres://task_user:task_password@localhost:5432/task_manager"
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	defer db.Close()
 
-	e.Use(middleware.CORS())
+	// Ping the database to check if it's alive
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping the database: %v", err)
+	}
+
+	e := echo.New()
 
 	// Initialize a new Service
 	service := NewService()
 
 	// Register routes
 	service.RegisterRoutes(e)
+
+	// Middleware
+	e.Use(middleware.CORS())
+	e.Use(middleware.Recover())
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus: true,
+		LogURI:    true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			service.infoLog.Printf("REQUEST: uri: %v, status: %v\n", v.URI, v.Status)
+			return nil
+		},
+	}))
 
 	// Start the server
 	e.Logger.Fatal(e.Start(":8080"))
--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,10 @@ require github.com/labstack/echo v3.3.10+incompatible
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.4 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/labstack/echo/v4 v4.13.3 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -14,6 +18,7 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
